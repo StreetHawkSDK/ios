06b49a5da1c12955b634c8b218c9fcd3f067ec6a
ChristineXYS <flychristine@gmail.com>
2015-09-30 10:16:52 +0800

    New device release, update model to include the new models.

--- StreetHawk/Classes/ThirdParty/UIDevice_Extension/SHUIDevice-Hardware.m
+++ StreetHawk/Classes/ThirdParty/UIDevice_Extension/SHUIDevice-Hardware.m
@@ -65,6 +65,10 @@
         return @"iPhone 6";
     if ([platform hasPrefix:@"iPhone7,1"])
         return @"iPhone 6 Plus";
+    if ([platform hasPrefix:@"iPhone8,1"])
+        return @"iPhone 6s";
+    if ([platform hasPrefix:@"iPhone8,2"])
+        return @"iPhone 6s Plus";
     
     // iPod
     if ([platform hasPrefix:@"iPod1,1"])
@@ -77,6 +81,8 @@
         return @"iPod touch 4G";
     if ([platform hasPrefix:@"iPod5,1"])
         return @"iPod touch 5G";
+    if ([platform hasPrefix:@"iPod7,1"])
+        return @"iPod touch 6G";
 
     // iPad
     if ([platform hasPrefix:@"iPad1,1"])
@@ -89,16 +95,28 @@
         return @"iPad 4";
     if ([platform hasPrefix:@"iPad4,1"] || [platform hasPrefix:@"iPad4,2"] || [platform hasPrefix:@"iPad4,3"])
         return @"iPad Air";
+    if ([platform hasPrefix:@"iPad5,3"] || [platform hasPrefix:@"iPad5,4"])
+        return @"iPad Air 2";
     if ([platform hasPrefix:@"iPad2,5"] || [platform hasPrefix:@"2,6"] || [platform hasPrefix:@"2,7"])
         return @"iPad mini 1G";
     if ([platform hasPrefix:@"iPad4,4"] || [platform hasPrefix:@"iPad4,5"] || [platform hasPrefix:@"iPad4,6"])
         return @"iPad mini 2G";
+    if ([platform hasPrefix:@"iPad4,7"] || [platform hasPrefix:@"iPad4,8"] || [platform hasPrefix:@"iPad4,9"])
+        return @"iPad mini 3";
+    if ([platform hasPrefix:@"iPad5,1"] || [platform hasPrefix:@"iPad5,2"])
+        return @"iPad mini 4";
     
     // Apple TV
     if ([platform hasPrefix:@"AppleTV2,1"])
         return @"Apple TV 2G";
     if ([platform hasPrefix:@"AppleTV3,1"] || [platform hasPrefix:@"AppleTV3,2"])
         return @"Apple TV 3G";
+    if ([platform hasPrefix:@"AppleTV5,3"])
+        return @"Apple TV 4G";
+    
+    // Apple Watch
+    if ([platform hasPrefix:@"Watch1,1"] || [platform hasPrefix:@"Watch1,2"])
+        return @"Apple Watch";
     
     // Simulator
     if ([platform hasSuffix:@"86"] || [platform isEqual:@"x86_64"])
