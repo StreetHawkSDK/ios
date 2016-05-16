94abe5191f5582e3990e9b3b21e978391a4976bf
ChristineXYS <christine@streethawk.com>
2016-05-16 15:23:20 +0800

    Implement feed object. In new design there is only json feed, remove other types.

--- StreetHawk/Classes/Feed/Publish/SHFeedObject.h
+++ StreetHawk/Classes/Feed/Publish/SHFeedObject.h
@@ -29,20 +29,8 @@ typedef void (^SHNewFeedsHandler)();
  */
 typedef void (^SHFeedsFetchHandler)(NSArray *arrayFeeds, NSError *error);
 
-typedef enum
-{
-    /** News feed */
-    SHFeedType_News,
-    /** Offer feed */
-    SHFeedType_Offer,
-    /** Custom json feed */
-    SHFeedType_Json,
-    /** Unknown feed */
-    SHFeedType_Unknown,
-} SHFeedType;
-
 /**
- Base class for feed object.
+ Feed object definition.
  */
 @interface SHFeedObject : NSObject
 
@@ -52,19 +40,24 @@ typedef enum
 @property (nonatomic) NSInteger feed_id;
 
 /**
- Type of the feed item.
+ Title of this feed.
  */
-@property (nonatomic) SHFeedType type;
+@property (nonatomic, strong) NSString *title;
 
 /**
- Whether the Feed item is public or not. Public items are visible to all users. Non-public Feed items (templates) can be used in Campaign Actions or be added via assigning feed items.
+ Message of this feed.
  */
-@property (nonatomic) BOOL isPublic;
+@property (nonatomic, strong) NSString *message;
 
 /**
- Indicates if this item has been removed.
+ Campaign this feed belongs to.
  */
-@property (nonatomic) BOOL isDeleted;
+@property (nonatomic, strong) NSString *campaign;
+
+/**
+ Json content, parse to NSObject.
+ */
+@property (nonatomic, strong) id content;
 
 /**
  A timestamp when the item activates (it will not be visible to clients before). It's possible to be nil.
@@ -87,81 +80,13 @@ typedef enum
 @property (nonatomic, strong) NSDate *modified;
 
 /**
- Create from dictionary with filled properties, according to `type` it can create concrete object.
+ When the Feed item was deleted.
+ */
+@property (nonatomic, strong) NSDate *deleted;
+
+/**
+ Create from dictionary with filled properties.
  */
 + (SHFeedObject *)createFromDictionary:(NSDictionary *)dict;
-
-@end
-
-/**
- News feed object.
- */
-@interface SHFeedNewsObject : SHFeedObject
-
-/**
- Title of this news.
- */
-@property (nonatomic, strong) NSString *title;
-
-/**
- Message of this news.
- */
-@property (nonatomic, strong) NSString *message;
-
-@end
-
-/**
- Offer feed object.
- */
-@interface SHFeedOfferObject : SHFeedObject
-
-/**
- Title of this offer.
- */
-@property (nonatomic, strong) NSString *title;
-
-/**
- Description of this offer.
- */
-@property (nonatomic, strong) NSString *desc;
-
-/**
- Discount of this offer.
- */
-@property (nonatomic, strong) NSString *discount;
-
-/**
- Image_url of this offer.
- */
-@property (nonatomic, strong) NSString *image_url;
-
-@end
-
-/**
- Json feed object.
- */
-@interface SHFeedJsonObject : SHFeedObject
-
-/**
- Json content, parse to NSObject.
- */
-@property (nonatomic, strong) id content;
-
-@end
-
-/**
- Unknown feed object.
- */
-@interface SHFeedUnknownObject : SHFeedObject
-
-/**
- Content part, but type is not "json".
- */
-@property (nonatomic, strong) id content;
-
-/**
- Type read from dictionary.
- */
-@property (nonatomic, strong) NSString *typeString;
 
 @end
--- StreetHawk/Classes/Feed/Publish/SHFeedObject.m
+++ StreetHawk/Classes/Feed/Publish/SHFeedObject.m
@@ -21,102 +21,51 @@
 
 @implementation SHFeedObject
 
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"feed id = %d, title = %@, message = %@, campaign = %@, content = %@, activate on %@, expires on %@, created on %@, modified on %@, deleted on %@", self.feed_id, self.title, self.message, self.campaign, self.content, shFormatStreetHawkDate(self.activates), shFormatStreetHawkDate(self.expires), shFormatStreetHawkDate(self.created), shFormatStreetHawkDate(self.modified), shFormatStreetHawkDate(self.deleted)];
+}
+
 + (SHFeedObject *)createFromDictionary:(NSDictionary *)dict
 {
-    SHFeedObject *obj = nil;
-    NSObject *typeVal = dict[@"type"];
-    NSString *typeStr = nil;
-    NSAssert(typeVal != nil && [typeVal isKindOfClass:[NSString class]], @"Type is not string for feed: %@.", dict);
-    if (typeVal != nil && [typeVal isKindOfClass:[NSString class]])
-    {
-        typeStr = (NSString *)typeVal;
-    }
+    SHFeedObject *obj = [[SHFeedObject alloc] init];
+    obj.feed_id = [dict[@"id"] integerValue];
     NSObject *contentVal = dict[@"content"];
+    NSAssert(contentVal != nil && [contentVal isKindOfClass:[NSDictionary class]], @"content should be dictionary.");
     if (contentVal != nil && [contentVal isKindOfClass:[NSDictionary class]])
     {
         NSDictionary *contentDict = (NSDictionary *)contentVal;
-        if (typeStr != nil && typeStr.length > 0 && [typeStr compare:@"news" options:NSCaseInsensitiveSearch] == NSOrderedSame)
+        NSObject *apsVal = contentDict[@"aps"];
+        NSAssert(apsVal != nil && [apsVal isKindOfClass:[NSDictionary class]], @"aps should be dictionary.");
+        if (apsVal != nil && [apsVal isKindOfClass:[NSDictionary class]])
         {
-            obj = [[SHFeedNewsObject alloc] init];
-            obj.type = SHFeedType_News;
-            SHFeedNewsObject *newsObj = (SHFeedNewsObject *)obj;
-            newsObj.title = contentDict[@"title"];
-            newsObj.message = contentDict[@"message"];
+            NSDictionary *apsDict = (NSDictionary *)apsVal;
+            NSString *alert = apsDict[@"alert"];
+            int length = [contentDict[@"l"] intValue];
+            if (length >= 0 && length < alert.length)
+            {
+                obj.title = [[alert substringToIndex:length] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                obj.message = [[alert substringFromIndex:length] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            }
         }
-        else if (typeStr != nil && typeStr.length > 0 && [typeStr compare:@"offer" options:NSCaseInsensitiveSearch] == NSOrderedSame)
+        NSObject *data = contentDict[@"d"];
+        NSDictionary *dataDict = shParseObjectToDict(data);
+        if (dataDict != nil)
         {
-            obj = [[SHFeedOfferObject alloc] init];
-            obj.type = SHFeedType_Offer;
-            SHFeedOfferObject *offerObj = (SHFeedOfferObject *)obj;
-            offerObj.title = contentDict[@"title"];
-            offerObj.desc = contentDict[@"description"];
-            offerObj.discount = contentDict[@"discount"];
-            offerObj.image_url = contentDict[@"image_url"];
-        }
-    }
-    if (obj == nil)
-    {
-        if (typeStr != nil && typeStr.length > 0 && [typeStr compare:@"json" options:NSCaseInsensitiveSearch] == NSOrderedSame)
-        {
-            obj = [[SHFeedJsonObject alloc] init];
-            obj.type = SHFeedType_Json;
-            SHFeedJsonObject *jsonObj = (SHFeedJsonObject *)obj;
-            jsonObj.content = contentVal;
+            obj.content = dataDict;
         }
         else
         {
-            obj = [[SHFeedUnknownObject alloc] init];
-            obj.type = SHFeedType_Unknown;
-            SHFeedUnknownObject *unknownObj = (SHFeedUnknownObject *)obj;
-            unknownObj.content = contentVal;
-            unknownObj.typeString = typeStr;
+            obj.content = data;
         }
     }
-    NSAssert(obj != nil, @"Fail to create feed object from dict: %@.", dict);
-    obj.feed_id = [dict[@"id"] integerValue];
-    obj.isPublic = ([dict[@"public"] integerValue] == 1);
-    obj.isDeleted = ([dict[@"deleted"] integerValue] == 1);
+    obj.campaign = dict[@"campaign"];
     obj.activates = shParseDate(dict[@"activates"], 0);
     obj.expires = shParseDate(dict[@"expires"], 0);
     obj.created = shParseDate(dict[@"created"], 0);
     obj.modified = shParseDate(dict[@"modified"], 0);
+    obj.deleted = shParseDate(dict[@"deleted"], 0);
     return obj;
-}
-
-@end
-
-@implementation SHFeedNewsObject
-
-- (NSString *)description
-{
-    return [NSString stringWithFormat:@"News feed, title = %@, message = %@.", self.title, self.message];
-}
-
-@end
-
-@implementation SHFeedOfferObject
-
-- (NSString *)description
-{
-    return [NSString stringWithFormat:@"Offer feed, title = %@, description = %@, discount = %@, image url = %@.", self.title, self.desc, self.discount, self.image_url];
-}
-
-@end
-
-@implementation SHFeedJsonObject
-
-- (NSString *)description
-{
-    return [NSString stringWithFormat:@"Json feed, content = %@.", self.content];
-}
-
-@end
-
-@implementation SHFeedUnknownObject
-
-- (NSString *)description
-{
-    return [NSString stringWithFormat:@"Unsupported feed, type = %@, content = %@.", self.typeString, self.content];
 }
 
 @end
