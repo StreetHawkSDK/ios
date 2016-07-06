da95380b888ccd38051f194a84559e364aa3b22d
ChristineXYS <christine@streethawk.com>
2016-07-06 17:59:56 +0800

    If customer not plugin Locations module, Geofence module sends logline 19 and 20 every one hour to optimise server's geofence/tree generation. If Location module is plugin, Geofence module does nothing.

--- StreetHawk/Classes/Location/Private/SHGeofenceBridge.m
+++ StreetHawk/Classes/Location/Private/SHGeofenceBridge.m
@@ -19,12 +19,20 @@
 //header from StreetHawk
 #import "SHApp+Location.h"
 #import "SHLocationManager.h"
+#import "SHLogger.h" //for sendLogForCode
 #import "SHGeofenceStatus.h"
+
+#define SH_GEOFENCE_LATLNG_SENTTIME @"SH_GEOFENCE_LATLNG_SENTTIME" //timestamp for recording last lat/lng sent time
 
 @interface SHGeofenceBridge ()
 
 + (void)createLocationManagerHandler:(NSNotification *)notification;
 + (void)setGeofenceTimestampHandler:(NSNotification *)notification; //handle app_status set geofence timestamp. notification name: SH_LMBridge_SetGeofenceTimestamp; user info: @{@"timestamp": NONULL(geofenceTimestamp)}.
+
++ (void)startMonitorGeoLocationHandler:(NSNotification *)notification; //for start location monitor for lat/lng. notification name: SH_LMBridge_StartMonitorGeoLocation; user info: empty.
++ (void)stopMonitorGeoLocationHandler:(NSNotification *)notification; //for stop location monitor for lat/lng. notification name: SH_LMBridge_StopMonitorGeoLocation; user info: empty.
++ (void)updateGeolocationCacheHandler:(NSNotification *)notification; //for updating local NSUserDefaults to pass values between modules, use notification to update when necessary, not refresh local cache too often. notification name: SH_LMBridge_UpdateGeoLocation; user info: empty.
++ (void)sendGeolocationUpdateHandler:(NSNotification *)notification; //for sending logline 20. notification name: SH_LMBridge_SendGeoLocationLogline; user info: @{comment: <json_comment>}.
 
 @end
 
@@ -37,6 +45,10 @@
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(createLocationManagerHandler:) name:@"SH_LMBridge_CreateLocationManager" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setGeofenceTimestampHandler:) name:@"SH_LMBridge_SetGeofenceTimestamp" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(startMonitorGeoLocationHandler:) name:@"SH_LMBridge_StartMonitorGeoLocation" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(stopMonitorGeoLocationHandler:) name:@"SH_LMBridge_StopMonitorGeoLocation" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateGeolocationCacheHandler:) name:@"SH_LMBridge_UpdateGeoLocation" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sendGeolocationUpdateHandler:) name:@"SH_LMBridge_SendGeoLocationLogline" object:nil];
 }
 
 #pragma mark - private functions
@@ -53,6 +65,56 @@
 {
     NSString *timestamp = notification.userInfo[@"timestamp"];
     [SHGeofenceStatus sharedInstance].geofenceTimestamp = timestamp;
+}
+
++ (void)startMonitorGeoLocationHandler:(NSNotification *)notification
+{
+    //Geofence does not need location latitude/longitude (log code 19, 20) actually, but server would like to have 19 and 20 to help generate geofence tree.
+    if (NSClassFromString(@"SHLocationBridge") == nil) //Geofence only do this when Locations module is absent.
+    {
+        [StreetHawk.locationManager startMonitorGeoLocationStandard:NO]; //Geofence always uses significant location change to reduce cost.
+    }
+}
+
++ (void)stopMonitorGeoLocationHandler:(NSNotification *)notification
+{
+    if (NSClassFromString(@"SHLocationBridge") == nil) //Geofence only do this when Locations module is absent.
+    {
+        [StreetHawk.locationManager stopMonitorGeoLocation];
+    }
+}
+
++ (void)updateGeolocationCacheHandler:(NSNotification *)notification
+{
+    if (NSClassFromString(@"SHLocationBridge") == nil) //Geofence only do this when Locations module is absent.
+    {
+        [[NSUserDefaults standardUserDefaults] setObject:@(StreetHawk.locationManager.currentGeoLocation.latitude) forKey:SH_GEOLOCATION_LAT];
+        [[NSUserDefaults standardUserDefaults] setObject:@(StreetHawk.locationManager.currentGeoLocation.longitude) forKey:SH_GEOLOCATION_LNG];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+}
+
++ (void)sendGeolocationUpdateHandler:(NSNotification *)notification
+{
+    if (NSClassFromString(@"SHLocationBridge") == nil) //Geofence only do this when Locations module is absent.
+    {
+        //Geofence lat/lng logline 19, 20 is sent every one hour.
+        NSObject *sentTime = [[NSUserDefaults standardUserDefaults] objectForKey:SH_GEOFENCE_LATLNG_SENTTIME];
+        if (sentTime != nil && [sentTime isKindOfClass:[NSNumber class]])
+        {
+            double sentTimestamp = [(NSNumber *)sentTime doubleValue];
+            if ([[NSDate date] timeIntervalSince1970] - sentTimestamp < 60 * 60/*one hour*/)
+            {
+                return;
+            }
+        }
+        NSString *comment = notification.userInfo[@"comment"];
+        NSAssert(comment != nil, @"\"comment\" in sendGeolocationUpdateHandler should not be nil.");
+        [StreetHawk sendLogForCode:LOG_CODE_LOCATION_MORE withComment:comment]; //send logline 19
+        [StreetHawk sendLogForCode:LOG_CODE_LOCATION_GEO withComment:comment]; //send logline 20
+        [[NSUserDefaults standardUserDefaults] setObject:@([[NSDate date] timeIntervalSince1970]) forKey:SH_GEOFENCE_LATLNG_SENTTIME];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
 }
 
 @end
