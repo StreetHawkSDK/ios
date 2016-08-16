2ca0cc65eea8fea01f47efb25cd4a8efe5c6914e
ChristineXYS <christine@streethawk.com>
2016-08-16 11:24:37 +0800

    Finish DeepLinkingViewController.

---
+++ Example_Swift/Demo/Notification/DeepLinkingViewController.swift
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) StreetHawk, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+import UIKit
+
+class DeepLinkingViewController: StreetHawkBaseViewController
+{
+    @IBOutlet var labelParam: UILabel!
+    
+    var dictParam : NSDictionary = [:]
+    
+    //life cycle
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?)
+    {
+        super.init(nibName: "DeepLinkingViewController"/*must explict write name, use nil not load correct xib in iOS 7*/, bundle: nibBundleOrNil)
+    }
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        self.title = "Show Deeplinking Param"
+    }
+
+    //deeplinking handler
+    
+    override func receiveDeepLinkingData(dictParam: [NSObject : AnyObject]!)
+    {
+        self.dictParam = dictParam
+        self.displayDeepLinkingToUI()
+    }
+    
+    override func displayDeepLinkingToUI()
+    {
+        self.labelParam.text = "\(self.dictParam)"
+    }
+}
