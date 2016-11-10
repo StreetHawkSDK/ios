f5c5ec3b59b8a7afa0e060d4c3cc7ba7a77b4e61
ChristineXYS <christine@streethawk.com>
2016-11-10 16:44:49 +0800

    Add pointzi binary framework to pod. Next is to change pod spec to include this.

---
+++ StreetHawk/Classes/Pointzi/StreetHawkCore_Pointzi.framework/Headers/SHPointzi.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) StreetHawk, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+#import <Foundation/Foundation.h>
+
+/**
+ Xamarin needs to have an object to map. It's impossible to map to SHApp because Core module has taken it. Create a new one for Pointzi module.
+ */
+@interface SHPointzi : NSObject
+
+/**
+ Singleton creator of SHPointzi.
+ @return Singleton SHPointzi instance.
+ */
++ (SHPointzi *)sharedInstance;
+
+@end
---
+++ StreetHawk/Classes/Pointzi/StreetHawkCore_Pointzi.framework/Headers/StreetHawkCore_Pointzi.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) StreetHawk, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreetHawkCore_Pointzi.
+FOUNDATION_EXPORT double StreetHawkCore_PointziVersionNumber;
+
+//! Project version string for StreetHawkCore_Pointzi.
+FOUNDATION_EXPORT const unsigned char StreetHawkCore_PointziVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreetHawkCore_Pointzi/PublicHeader.h>
+
+#import <StreetHawkCore_Pointzi/PushDataForApplication.h>
+#import <StreetHawkCore_Pointzi/SHApp.h>
+#import <StreetHawkCore_Pointzi/SHBaseViewController.h>
+#import <StreetHawkCore_Pointzi/SHFriendlyNameObject.h>
+#import <StreetHawkCore_Pointzi/SHInstall.h>
+#import <StreetHawkCore_Pointzi/SHObject.h>
+#import <StreetHawkCore_Pointzi/SHTypes.h>
+
+#import <StreetHawkCore_Pointzi/SHApp+Pointzi.h>
+#import <StreetHawkCore_Pointzi/SHTooltip.h>
+#import <StreetHawkCore_Pointzi/SHPointzi.h>
