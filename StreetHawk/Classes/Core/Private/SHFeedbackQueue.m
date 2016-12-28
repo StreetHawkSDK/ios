44abd07228cd34efd77b662b99e12998bdf748c1
ChristineXYS <christine@streethawk.com>
2016-12-28 16:12:20 +0800

    Feedback uses orientation too. Now it displays well when rotating.

--- StreetHawk/Classes/Core/Private/SHFeedbackQueue.m
+++ StreetHawk/Classes/Core/Private/SHFeedbackQueue.m
@@ -185,7 +185,10 @@
             SHFeedbackViewController *feedbackVC = [[SHFeedbackViewController alloc] initWithNibName:nil bundle:nil];
             feedbackVC.inputHandler = inputHandler;
             feedbackVC.feedbackTitle = feedbackChoice;
-            [feedbackVC presentOnTopWithCover:YES withCoverColor:nil withCoverAlpha:0 withCoverTouchHandler:nil withAnimationHandler:nil];
+            [feedbackVC presentOnTopWithCover:YES withCoverColor:nil withCoverAlpha:0 withCoverTouchHandler:nil withAnimationHandler:nil withOrientationChangedHandler:^(CGRect fullScreenRect)
+             {
+                 feedbackVC.view.frame = CGRectMake((fullScreenRect.size.width - feedbackVC.view.frame.size.width) / 2, (fullScreenRect.size.height - feedbackVC.view.frame.size.height) / 2, feedbackVC.view.frame.size.width, feedbackVC.view.frame.size.height);
+             }];
         };
         //arrayChoice may contain none string, or empty string, need to filter out otherwise cause crash.
         NSMutableArray *arrayChoiceRefine = [NSMutableArray array];
@@ -302,7 +305,10 @@
             [choiceVC presentOnTopWithCover:YES withCoverColor:nil withCoverAlpha:0 withCoverTouchHandler:nil withAnimationHandler:^(CGRect fullScreenRect)
              {
                  [choiceVC arrangeControls:fullScreenRect];
-             }];
+             } withOrientationChangedHandler:^(CGRect fullScreenRect)
+            {
+                [choiceVC arrangeControls:fullScreenRect];
+            }];
         }
     }
 }
