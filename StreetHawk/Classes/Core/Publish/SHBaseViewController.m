f441f22fdc1aefc3b99e492c46df490253ea1f07
ChristineXYS <christine@streethawk.com>
2017-02-23 16:42:16 +0800

    Support super tag in Pointzi.

--- StreetHawk/Classes/Core/Publish/SHBaseViewController.m
+++ StreetHawk/Classes/Core/Publish/SHBaseViewController.m
@@ -59,6 +59,7 @@
         [self performSelector:@selector(displayDeepLinkingToUI)];
     }
     [[NSNotificationCenter defaultCenter] postNotificationName:@"SH_PointziBridge_ShowTip_Notification" object:nil userInfo:@{@"vc": self}];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"SH_PointziBridge_SuperTag_Notification" object:nil userInfo:@{@"vc": self}];
 }
 
 //tricky: Record `viewWillAppear` as backup, become in canceled pop up `viewDidAppear` is not called.
@@ -106,7 +107,8 @@
     {
         [self performSelector:@selector(displayDeepLinkingToUI)];
     }
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"SH_PointziBridge_ShowTip_Notification" object:nil userInfo:@{@"vc": self}];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"SH_PointziBridge_ShowTip_Notification" object:nil userInfo:@{@"vc": self}];    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"SH_PointziBridge_SuperTag_Notification" object:nil userInfo:@{@"vc": self}];
 }
 
 - (void)viewWillAppear:(BOOL)animated
