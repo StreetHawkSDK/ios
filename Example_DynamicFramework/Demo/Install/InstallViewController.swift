593365c7d25bf4a4cec989c33f5d15635c01833f
ChristineXYS <christine@streethawk.com>
2017-08-28 16:12:51 +0800

    Enable “use_frameworks!” for dynamic framework part. Meet 2 errors: 1. Swift file cannot find StreetHawkBaseViewController and SHApp.     — fixed by adding: import StreetHawkCore 2. Project pod compile error:     ld: '/Users/h238701/Projects/Test/Hawk/ios/StreetHawk/Vendor/CrashReporter.framework/CrashReporter(libCrashReporter-iphoneos.a-arm64-master.o)' does not contain bitcode. You must rebuild it with bitcode enabled (Xcode setting ENABLE_BITCODE), obtain an updated library from the vendor, or disable bitcode for this target. for architecture arm64 clang: error: linker command failed with exit code 1 (use -v to see invocation)     Test if set bitcode=NO in pod project streethawk, it can fix the error.     Need to find a way so that auto do “pod install” can configure it well.

--- Example_DynamicFramework/Demo/Install/InstallViewController.swift
+++ Example_DynamicFramework/Demo/Install/InstallViewController.swift
@@ -16,6 +16,7 @@
  */
 
 import UIKit
+import StreetHawkCore
 
 class InstallViewController: StreetHawkBaseTableViewController
 {
--- Example_DynamicFramework/Demo/Swift/SwiftViewController.swift
+++ Example_DynamicFramework/Demo/Swift/SwiftViewController.swift
@@ -16,6 +16,7 @@
 */
 
 import UIKit
+import StreetHawkCore
 
 class SwiftViewController: StreetHawkBaseViewController
 {
