590a2ed50927a2637f3a767ed58b286f6b364c74
ChristineXYS <christine@streethawk.com>
2017-08-29 14:30:41 +0800

    Combine two build.sh together and move to top level.

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -x
+
+# delete /build/outputs folder
+rm -Rf ./build/outputs/*
+pushd .
+
+# ------------------- build SHStatic ------------------------
+
+cd Example_StaticLibrary
+
+# install third-party pods
+pod install
+
+# clean project
+xcodebuild clean -workspace StreetHawkDemo.xcworkspace -scheme StreetHawkDemo -sdk iphoneos -configuration Release
+
+# archive app
+xcodebuild archive -workspace StreetHawkDemo.xcworkspace -scheme StreetHawkDemo -archivePath ../build/outputs/SHStatic.xcarchive
+
+# export ipa
+xcodebuild -exportArchive -archivePath ../build/outputs/SHStatic.xcarchive -exportPath ../build/outputs/ -exportOptionsPlist ../build/ExportPlist.plist
+mv ../build/outputs/StreetHawkDemo.ipa ../build/outputs/SHStatic.ipa
+
+# ------------------- build SHDynamic ------------------------
+
+cd ..
+cd Example_DynamicFramework
+
+# install third-party pods
+pod install
+
+# clean project
+xcodebuild clean -workspace StreetHawkDemo.xcworkspace -scheme StreetHawkDemo -sdk iphoneos -configuration Release
+
+# archive app
+xcodebuild archive -workspace StreetHawkDemo.xcworkspace -scheme StreetHawkDemo -archivePath ../build/outputs/SHDynamic.xcarchive
+
+# export ipa
+xcodebuild -exportArchive -archivePath ../build/outputs/SHDynamic.xcarchive -exportPath ../build/outputs/ -exportOptionsPlist ../build/ExportPlist.plist
+mv ../build/outputs/StreetHawkDemo.ipa ../build/outputs/SHDynamic.ipa
+
+popd
+
