1e0fc9447fcff27a87b8993620b175a49c5b8159
ChristineXYS <christine@streethawk.com>
2017-08-29 11:47:23 +0800

    Write build.sh for SHStatic.

--- Example_ObjC/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -x
-pod install
-xcodebuild -workspace StreetHawkDemo.xcworkspace -scheme StreetHawkDemo -sdk iphoneos -configuration Release
---
+++ Example_StaticLibrary/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+# install third-party pods
+pod install
+
+# delete /build/outputs folder
+rm -Rf /build/outputs/*
+
+# clean project
+xcodebuild clean -workspace StreetHawkDemo.xcworkspace -scheme StreetHawkDemo -sdk iphoneos -configuration Release
+
+# archive app
+xcodebuild archive -workspace StreetHawkDemo.xcworkspace -scheme StreetHawkDemo -archivePath ./build/outputs/SHStatic.xcarchive
+
+# export ipa
+xcodebuild -exportArchive -archivePath ./build/outputs/SHStatic.xcarchive -exportPath ./build/outputs/SHStatic.ipa -exportOptionsPlist ./build/ExportPlist.plist
+
