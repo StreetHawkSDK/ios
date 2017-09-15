e0d8a360beb1f43785e8ee16454aa8ac1c9775a1
ChristineXYS <christine@streethawk.com>
2017-09-16 00:40:01 +0800

    Fix ANDROID-503: Applying MD5 hash logic of String - iOS Make md5 a utility function.

--- StreetHawk/Classes/Core/Publish/SHUtils.m
+++ StreetHawk/Classes/Core/Publish/SHUtils.m
@@ -1026,3 +1026,22 @@ static NSString *getPropertyType(objc_property_t property)
 }
 
 @end
+
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString (SHExt)
+
+- (NSString *)md5
+{
+    const char *cStr = [[NSData dataWithContentsOfFile:self] bytes];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    NSInteger length = [[NSData dataWithContentsOfFile:self] length]; // strlen(cStr);
+    CC_MD5(cStr, (int)length, result);
+    return [NSString stringWithFormat:
+            @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+            result[0], result[1], result[2], result[3], result[4], result[5],
+            result[6], result[7], result[8], result[9], result[10], result[11],
+            result[12], result[13], result[14], result[15]];
+}
+
+@end
