1f5de57ada2d3a0c405b09b12825beee9970f612
ChristineXYS <christine@streethawk.com>
2018-02-07 00:35:58 +0800

    Fix IOS-1213: Sample App's tag page is mess Fix table cell’s layout by assign suitable height.

--- Example_DynamicFramework/Demo/LogTag/LogTagCasesViewController.m
+++ Example_DynamicFramework/Demo/LogTag/LogTagCasesViewController.m
@@ -32,7 +32,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.arrayCells = @[self.cellCuid, self.cellNumeric, self.cellString, self.cellDatetime, self.cellIncrement, self.cellIncrementValue, self.cellDelete, self.cellFeed];
+    self.arrayCells = @[self.cellCuid, self.cellNumeric, self.cellString, self.cellDatetime, self.cellIncrement, self.cellIncrementValue, self.cellDelete, self.cellFeed, self.cellError];
 }
 
 #pragma mark - Table view data source
@@ -49,13 +49,26 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = self.arrayCells[indexPath.row];
-    return cell.bounds.size.height;
+    if (indexPath.row == 0
+        || indexPath.row == 4
+        || indexPath.row == 6)
+    {
+        return 80;
+    }
+    else if (indexPath.row == 7
+             || indexPath.row == 8)
+    {
+        return 50;
+    }
+    else
+    {
+        return 135;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return self.arrayCells[indexPath.row];;
+    return self.arrayCells[indexPath.row];
 }
 
 #pragma mark - UITextFieldDelegate handler
@@ -234,6 +247,12 @@
              }
          });
      }];
+}
+
+- (IBAction)buttonErrorClicked:(id)sender
+{
+    [StreetHawk sendLogForCode:LOG_CODE_ERROR withComment:@"Test error logline."];
+    [self showDoneAlert:YES];
 }
 
 #pragma mark - private functions
